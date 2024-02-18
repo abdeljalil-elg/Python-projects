96ee8d64f09295f47cdb5b2c7607f0e37f9ea38c
abdeljalil-elg <156678084+abdeljalil-elg@users.noreply.github.com>
2024-02-18 19:07:11 +0100

    Update Limite.py

--- limite-dv-primitive/Limite.py
+++ limite-dv-primitive/Limite.py
@@ -1,11 +1,16 @@
+#abdeljalilelgadi
+#how to calculate limit of function.
+
+
+
 import sympy as sp
 
-def calculer_limite(fonction, point):
+def ma_limite(fct, point):
     # Définit la variable symbolique
     x = sp.symbols('x')
     
     # Convertit la fonction entrée en une expression sympy
-    expression = sp.sympify(fonction)
+    expression = sp.sympify(fct)
     
     # Calcule la limite de la fonction en un point
     limite = sp.limit(expression, x, point)
@@ -13,12 +18,12 @@ def calculer_limite(fonction, point):
     return limite
 
 def main():
-    fonction = input("Entrez la fonction (par exemple, 'x**2 + 1/x') : ")
+    fct = input("Entrez la fonction (par exemple, 'x**2 + 1/x') : ")
     point = input("Entrez le point pour lequel vous souhaitez calculer la limite (par exemple, 'oo' pour l'infini) : ")
     
     try:
-        limite = calculer_limite(fonction, point)
-        print("La limite de la fonction", fonction, "en", point, "est :", limite)
+        limite = ma_limite(fct, point)
+        print("La limite de la fonction", fct, "en", point, "est :", limite)
     except Exception as e:
         print("Une erreur s'est produite lors du calcul de la limite :", e)
 
